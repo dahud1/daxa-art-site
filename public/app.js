4062df56bc839b3845000054637e201761d9cb6b
Dahud Ali <dahudali@hotmail.com>
2023-03-10 23:49:03 +0000

    First commit

---
+++ public/app.js
@@ -0,0 +1,13 @@
+
+
+function exampleScroll() {
+    const element = document.getElementById("examples");
+    element.scrollIntoView();
+}
+
+
+function faqScroll() {
+    const element = document.getElementById("faqs");
+    element.scrollIntoView();
+}
+
